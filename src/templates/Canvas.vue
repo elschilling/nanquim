a7c25da4ba5e73298408e8e5b264de1dce52e8f9
Eduardo Schilling <elschilling@gmail.com>
2023-12-29 11:32:54 -0300

    Zoom extents

--- src/templates/Canvas.vue
+++ src/templates/Canvas.vue
@@ -4,276 +4,204 @@
     .wgt.wgt-menu
         span.icon.icon-editor-svgcad
         .icon.icon-dropdown
-  .layout-editor
+  .layout-editor#canvas
     //- canvas(ref='canvas' @click='handleClick' @contextmenu.prevent='stopDrawing' @mousedown="onPointerDown" @mousemove="onPointerMove" @mouseup="onPointerUp" @wheel="(e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY)")
-    svg(id='canvas' viewBox="-50 -50 100 100" ref='svg' stroke='red' stroke-width='.1'
-    @click='handleClick'
-    @contextmenu.prevent='stopDrawing'
-    @mousedown="onPointerDown"
-    @mousemove="onPointerMove"
-    @mouseup="onPointerUp"
-    @wheel.prevent="adjustZoom")
+    //- svg(id='canvas' viewBox="-10 -10 20 20" ref='svg'
+    //- @click='handleClick'
+    //- @contextmenu.prevent='stopDrawing'
+    //- @mousedown="onPointerDown"
+    //- @mousemove="onPointerMove"
+    //- @mouseup="onPointerUp"
+    //- @wheel.prevent="adjustZoom")
       //- circle(cx='0' cy='0' r='5')
       //- circle(cx='0' cy='0' r='4')
       //- circle(cx='0' cy='0' r='3')
       //- circle(cx='0' cy='0' r='2')
       //- circle(cx='0' cy='0' r='1')
-      line(x1="0" y1="-50" x2="0" y2="50" class='axis y-axis')
-      line(x1="-50" y1="0" x2="50" y2="0" class='axis x-axis')
-    Terminal(@inputAsk='capturePoint' @stopDrawing='stopDrawing' :coords='coordinates')
+      line(x1="0" y1="-5000000" x2="0" y2="5000000" class='axis y-axis')
+      line(x1="-5000000" y1="0" x2="5000000" y2="0" class='axis x-axis')
+    //- Terminal(@inputAsk='capturePoint' @stopDrawing='stopDrawing' :coords='coordinates')
 </template>
 
 <script setup>
-// PAN and ZOOM lifesave https://codepen.io/chengarda/pen/wRxoyB
 import { ref, onMounted, reactive } from 'vue'
-import { useEventBus, useMouse, useParentElement } from '@vueuse/core'
+import { useEventBus, useParentElement } from '@vueuse/core'
 import Terminal from './Terminal.vue'
 import { store } from '../store'
-// import { SVG } from '@svgdotjs/svg.js'
-// let draw = SVG().addTo('#canvas')
-// console.log('draw', draw)
-const svg = ref(null)
-let isDragging = false
-let dragStart = { x: 0, y: 0 }
-let viewBox
-let point
-var startClient
-let startGlobal
-let coordGlobal
-let zoomScaleFactor = 1.1
+import { SVG } from '@svgdotjs/svg.js'
+import '@svgdotjs/svg.panzoom.js'
+// import '../utils/svg.js/svg'
+// import '../utils/svg.draw'
+
+function zoomToFit(canvas, padding = 10) {
+  console.log('canvas', canvas.node.children[0])
+
+  const bbox = canvas.bbox()
+  console.log('bbox', bbox)
+  canvas.rect(bbox.width, bbox.height).stroke({ color: 'yellow', width: 0.2 }).fill({ opacity: 0.4 }).move(bbox.x, bbox.y)
+
+  const rbox = canvas.rbox()
+  console.log('rbox', rbox)
+  // canvas.rect(rbox.width, rbox.height).stroke({ color: 'blue', width: 0.2 }).fill({ opacity: 0.4 }).move(rbox.x, rbox.y)
+
+  let p = canvas.point({ x: -100, y: 0 })
+  // canvas.animate(300).zoom(zoomLevel)
+
+  console.log('zoomLevel', zoomLevel)
+  console.log('viewbox', viewbox)
+  // console.log('p', p)
+
+  // const newViewBox = {
+  //   x: -bbox.x,
+  //   y: -bbox.y,
+  //   width: bbox.width,
+  //   height: bbox.height,
+  // }
+  // console.log('viewboxSize', viewboxSize)
+  // canvas.viewbox(-viewboxSize * 0.3 + ' ' + -viewboxSize / 1.5 + ' ' + viewboxSize * 2 + ' ' + viewboxSize / 2)
+  canvas.animate(300).viewbox(bbox)
+}
+
+// SVG.extend(SVG.Dom, {
+//   zoomToFit: function (padding = 10) {
+//     const bbox = this.rbox()
+//     const viewbox = this.viewbox()
+
+//     const zoomX = viewbox.width / bbox.width
+//     const zoomY = viewbox.height / bbox.height
+//     const zoom = Math.min(zoomX, zoomY)
+
+//     const centerX = bbox.cx
+//     const centerY = bbox.cy
+
+//     const offsetX = centerX * zoom - viewbox.width / 2
+//     const offsetY = centerY * zoom - viewbox.height / 2
+
+//     this.animate(300).viewbox({
+//       x: offsetX - padding,
+//       y: offsetY - padding,
+//       width: viewbox.width * zoom + padding * 2,
+//       height: viewbox.height * zoom + padding * 2,
+//     })
+//   },
+// })
+
+let canvas
+let viewbox
+let viewboxSize = 10
+const coordinates = ref({ x: 0, y: 0 })
+let zoomFactor = 0.1
+let zoomLevel
+let lastMiddleClickTime = 0
+let middleClickCount = 0
+let GRID_SIZE = 20
+let GRID_SPACING = 1
 
 // Local events
 const emit = defineEmits(['coordsUpdated'])
 
 // Global events
 const newCoords = useEventBus('newCoords')
-const newPoint = useEventBus('newPoint')
-const startDrawing = useEventBus('startDrawing')
-const newDrawing = useEventBus('newDrawgin')
-let unsubNewDrawing
-
-let unsubStartDrawing
-const coordinates = ref({ x: 0, y: 0 })
-let drawings = []
-let context = reactive({})
-let cameraOffset = { x: 0, y: 0 }
-
-const parentEl = useParentElement()
-const extractor = (event) => {
-  if (typeof Touch !== 'undefined' && event instanceof Touch) {
-    return null
-  } else {
-    return [event.offsetX, event.offsetY]
-  }
-}
-const { x, y, sourceType } = useMouse({ target: parentEl, type: extractor })
-const mouse = { x: x, y: y }
-
-unsubStartDrawing = startDrawing.on((drawCommand) => {
-  if (unsubNewDrawing) unsubNewDrawing()
-  store.isDrawing = true
-  drawCommand()
-  unsubNewDrawing = newDrawing.on((drawingCommand, drawingMode) => {
-    if (drawingMode) {
-      activeDrawing = drawingCommand
-    } else {
-      drawings.push(drawingCommand)
-      activeDrawing = null
-    }
-  })
-})
+// const newPoint = useEventBus('newPoint')
+// const startDrawing = useEventBus('startDrawing')
+// const newDrawing = useEventBus('newDrawgin')
 
 onMounted(() => {
-  drawGrid(10, spacing)
-  viewBox = svg.value.viewBox.baseVal
-  console.log('viewBox', viewBox)
-  startClient = svg.value.createSVGPoint()
-  startGlobal = svg.value.createSVGPoint()
-  coordGlobal = svg.value.createSVGPoint()
-  point = svg.value.createSVGPoint()
-  // context = getCanvasContext()
-  // draw()
+  canvas = SVG()
+    .addTo('#canvas')
+    .addClass('canvas')
+    // .viewbox(-viewboxSize * 0.3 + ' ' + -viewboxSize / 1.5 + ' ' + viewboxSize * 2 + ' ' + viewboxSize / 2)
+    .panZoom({ zoomFactor, panButton: 1 })
+    .mousemove(updateCoordinates)
+    .mousedown(handleClick)
+  // .scale(1, -1)
+  viewbox = canvas.viewbox()
+  console.log('draw', canvas)
+  drawGrid(canvas, GRID_SIZE, GRID_SPACING)
+  drawAxis(canvas, GRID_SIZE)
+  canvas.rect(2, 5).move(3, -5)
+  canvas.rect(2, 5).move(3, 0)
+  canvas.rect(15, 15).move(-0.5, -0.5).fill({ color: 'teal', opacity: 0.5 }).stroke({ color: 'orange', width: 0.1 })
+  canvas.rect(1, 1).move(4.5, -0.5).fill({ color: 'teal', opacity: 0.5 }).stroke({ color: 'orange', width: 0.1 })
+  canvas.rect(1, 1).move(-4.5, -0.5).fill({ color: 'teal', opacity: 0.5 }).stroke({ color: 'orange', width: 0.1 })
+  canvas.text('svgcad').font({ size: 1 }).fill({ color: 'white' })
+  canvas.text('I ❤ SVG!').font({ size: 1, anchor: 'end' }).move(10, -1).fill({ color: 'red' })
+  canvas.line(0, 0, 5, -5).stroke({ color: 'white', width: 0.1 }).plot(0, -5, 5, 0).move(0, -10)
+  // canvas.zoom(1).animate(300).zoom(50)
+  canvas.animate(600).viewbox(canvas.bbox())
 })
 
-// function getCanvasContext() {
-//   return canvas.value?.getContext('2d')
-// }
-
-let spacing = 2
-let MAX_ZOOM = 5
-let MIN_ZOOM = 0.1
-let SCROLL_SENSITIVITY = 0.01
-let activeDrawing
-let isCapturingInput = false
-
-// function resize() {
-//   canvas.value.height = canvas.value.clientHeight - 4 // magic number!!!
-//   canvas.value.width = canvas.value.clientWidth
-// }
-
-// function cameraTranslate() {
-//   context.translate(canvas.value.width / 2, canvas.value.height / 2)
-//   context.scale(cameraZoom, -cameraZoom)
-//   context.translate(cameraOffset.x, -cameraOffset.y)
-//   clearCanvas(canvas, context)
-// }
-
-// function draw() {
-//   resize()
-//   cameraTranslate()
-//   context.strokeStyle = 'white'
-//   context.lineWidth = 1
-//   if (store.isDrawing) {
-//     if (activeDrawing) {
-//       clearCanvas(canvas, context)
-//       activeDrawing()
-//     }
-//   }
-//   drawings.forEach((drawingCommand) => drawingCommand())
-//   drawAxis()
-//   context.lineWidth = 0.5
-//   drawGrid()
-
-//   // insert new elements here
-
-//   // drawLine(context, -100, 0, 100, 0)
-//   // drawLine(context, 0, -100, 0, 100)
-//   // drawLine(context, -100, -100, 100, 100)
-//   // drawLine(context, 0, -100, 100, 0)
-//   // drawRect(context, 10, 10, 100, 100)
-//   context.beginPath()
-//   context.arc(0, 0, 40, 0, 2 * Math.PI)
-//   context.stroke()
-
-//   requestAnimationFrame(draw)
-// }
-
-function stopDrawing() {
-  //   clearCanvas(canvas, context)
-  //   store.isDrawing = false
-  //   isCapturingInput = false
-  //   activeDrawing = 0
-  //   unsubNewDrawing()
+function drawAxis(canvas, size) {
+  const axisGroup = canvas.group()
+  const xAxis = canvas.line(-size, 0, size, 0).addClass('axis x-axis').addTo(axisGroup)
+  const yAxis = canvas.line(0, size, 0, -size).addClass('axis y-axis').addTo(axisGroup)
 }
 
-function drawGrid(gridSize, spacing) {
-  const gridGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g')
-  gridGroup.setAttribute('stroke', '#4f4f4f')
-  gridGroup.setAttribute('stroke-width', '.1')
-  gridGroup.setAttribute('vector-effect', 'non-scaling-stroke')
-  gridGroup.setAttribute('class', 'grid')
-
-  for (let x = -gridSize; x <= gridSize; x += spacing) {
-    if (x != 0) {
-      const verticalLine = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-      verticalLine.setAttribute('x1', -gridSize * spacing)
-      verticalLine.setAttribute('y1', x * spacing)
-      verticalLine.setAttribute('x2', gridSize * spacing)
-      verticalLine.setAttribute('y2', x * spacing)
-      // verticalLine.setAttribute('class', 'grid')
-      gridGroup.appendChild(verticalLine)
-      const horizontalLine = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-      horizontalLine.setAttribute('x1', x * spacing)
-      horizontalLine.setAttribute('y1', -gridSize * spacing)
-      horizontalLine.setAttribute('x2', x * spacing)
-      horizontalLine.setAttribute('y2', gridSize * spacing)
-      gridGroup.appendChild(horizontalLine)
+function drawGrid(canvas, gridSize, spacing) {
+  const gridGroup = canvas.group()
+  gridGroup.addClass('grid')
+  for (let i = -gridSize; i <= gridSize; i += spacing) {
+    if (i != 0) {
+      const horizontalLines = canvas
+        .line(-gridSize * spacing, i * spacing, gridSize * spacing, i * spacing)
+        .addClass('axis')
+        .addTo(gridGroup)
+      const verticalLines = canvas
+        .line(i * spacing, -gridSize * spacing, i * spacing, gridSize * spacing)
+        .addClass('axis')
+        .addTo(gridGroup)
     }
   }
-
-  svg.value.appendChild(gridGroup)
 }
 
-// function drawGrid(svg) {
-//   // let steps = Math.ceil(canvas.value.width / GRID_SIZE / cameraZoom)
-//   const gridGroup = svg.group().attr({ class: 'grid' })
-//   let steps = 11
-//   context.strokeStyle = '#4f4f4f'
-//   for (let i = -steps; i < steps; i++) {
-//     if (i != 0) {
-//       const verticalLine = svg.line(-steps * GRID_SIZE, i * GRID_SIZE, (steps - 1) * GRID_SIZE, i * GRID_SIZE).addTo(gridGroup)
-//       // drawLine(getCanvasContext, -steps * GRID_SIZE, i * GRID_SIZE, (steps - 1) * GRID_SIZE, i * GRID_SIZE)
-//       // drawLine(getCanvasContext, i * GRID_SIZE, -steps * GRID_SIZE, i * GRID_SIZE, (steps - 1) * GRID_SIZE)
-//     }
-//   }
-// }
-
-function updateCoordinates() {
-  coordGlobal.x = mouse.x.value
-  coordGlobal.y = mouse.y.value
-  coordGlobal = coordGlobal.matrixTransform(svg.value.getScreenCTM().inverse())
-  coordinates.value = coordGlobal
-  emit('coordsUpdated', coordGlobal)
-  newCoords.emit(coordGlobal)
+function updateCoordinates(e) {
+  coordinates.value = canvas.point(e.pageX, e.pageY)
+  // TODO GRID SNAP TO AVOID THIS
+  coordinates.value.x = Math.round(coordinates.value.x)
+  coordinates.value.y = Math.round(coordinates.value.y)
+  emit('coordsUpdated', coordinates.value)
+  newCoords.emit(coordinates.value)
 }
 
-function onPointerDown(e) {
-  isDragging = true
-  startClient.x = e.x
-  startClient.y = e.y
-  startGlobal = startClient.matrixTransform(svg.value.getScreenCTM().inverse())
-}
-
-function onPointerMove(e) {
-  updateCoordinates()
-  if (isDragging) {
-    updateViewBox(e)
-  }
-}
-
-function onPointerUp(e) {
-  isDragging = false
-}
-
-function updateViewBox(e) {
-  point.x = e.x
-  point.y = e.y
-
-  var moveGlobal = point.matrixTransform(svg.value.getScreenCTM().inverse())
-
-  viewBox.x -= moveGlobal.x - startGlobal.x
-  viewBox.y -= moveGlobal.y - startGlobal.y
-}
-
-function adjustZoom(e) {
-  if (!isDragging) {
-    var normalized
-    var delta = e.wheelDelta
-    if (delta) {
-      normalized = delta % 120 == 0 ? delta / 120 : delta / 12
+function handleClick(e) {
+  console.log('canvas width', getComputedStyle(canvas.node.parentElement).width)
+  console.log('canvas height', getComputedStyle(canvas.node.parentElement).height)
+  const originalWidth = parseFloat(getComputedStyle(canvas.node.parentElement).width)
+  const originalHeight = parseFloat(getComputedStyle(canvas.node.parentElement).height)
+  console.log('viewbox', viewbox)
+  const zoomX = originalWidth / viewbox.width
+  const zoomY = originalHeight / viewbox.height
+  console.log('zoomX', zoomX)
+  console.log('zoomY', zoomY)
+  // zoomLevel = (zoomX + zoomY) / 2
+  zoomLevel = zoomX
+  console.log('zoomLevel', zoomLevel)
+  console.log('viewbox', canvas.viewbox())
+  // zoomLevel = viewboxSize / canvas.viewbox().height
+  console.log('zoomLvl', zoomLevel)
+  if (e.button === 1) {
+    const currentTime = new Date().getTime()
+    const timeDiff = currentTime - lastMiddleClickTime
+    if (timeDiff < 300) {
+      middleClickCount++
+      if (middleClickCount === 2) {
+        console.log('double midle click detected!!!')
+        zoomToFit(canvas, 1)
+        middleClickCount = 0
+      }
     } else {
-      delta = e.deltaY || e.detail || 0
-      normalized = -(delta % 3 ? delta * 10 : delta / 3)
+      middleClickCount = 1
     }
-    var scaleDelta = normalized > 0 ? 1 / zoomScaleFactor : zoomScaleFactor
-    point.x = e.clientX
-    point.y = e.clientY
-    var startPoint = point.matrixTransform(svg.value.getScreenCTM().inverse())
-    viewBox.x -= (startPoint.x - viewBox.x) * (scaleDelta - 1)
-    viewBox.y -= (startPoint.y - viewBox.y) * (scaleDelta - 1)
-    viewBox.width *= scaleDelta
-    viewBox.height *= scaleDelta
-  }
-}
-
-function capturePoint() {
-  isCapturingInput = true
-}
-
-async function handleClick(e) {
-  if (isCapturingInput) {
-    isCapturingInput = false
-    newPoint.emit(coordinates)
+    lastMiddleClickTime = currentTime
   }
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
 .editor-header
   border-top-left-radius: 20px
   border-top-right-radius: 20px
 .canvas-editor
-  cursor: crosshair
   border-radius: 20px
   box-sizing: border-box
   background-color: var(--node-editor-bg)
@@ -282,28 +210,27 @@ async function handleClick(e) {
   flex-flow: column
   flex-grow: 1
 .layout-editor
+  position: relative
   box-sizing: border-box
+  flex: 1
+  min-width: 600px
   width: 100%
   height: 100%
-  display: block
-svg
+.canvas
+  // preserveAspectRatio:
+  cursor: crosshair
   position: absolute
-  box-sizing: border-box
-  background: var(--canvas-bg)
-  overflow: hidden
   min-width: 600px
   width: 100%
   height: 100%
 
 .axis
   vector-effect: non-scaling-stroke
-  stroke-width: 2
+  stroke-width: 1
 .x-axis
   stroke: #597631
 .y-axis
   stroke: #843d47
 .grid
-  stroke-width: 2
-  stroke: blue
-  // vector-effect: non-scaling-stroke
+  stroke: #4f4f4f
 </style>
