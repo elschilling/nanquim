19e8ee6982876c382d4a1090a9c2982f0155f8b3
Eduardo Schilling <elschilling@gmail.com>
2024-01-13 11:26:38 -0300

    fix coordinates rounding

--- src/js/StatusBar.js
+++ src/js/StatusBar.js
@@ -4,8 +4,12 @@ function StatusBar(editor) {
   const signals = editor.signals
 
   signals.updatedCoordinates.add((coordinates) => {
-    coordX.textContent = Math.floor(coordinates.x)
-    coordY.textContent = Math.floor(coordinates.y)
+    // coordX.textContent = Math.floor(coordinates.x)
+    coordX.textContent = Math.round(coordinates.x)
+    // coordX.textContent = coordinates.x
+    // coordY.textContent = Math.floor(coordinates.y)
+    coordY.textContent = Math.round(coordinates.y)
+    // coordY.textContent = coordinates.y
   })
 }
 
