2d8cb1e10696b78e2b09c9a96d1183935592a33b
Eduardo Schilling <elschilling@gmail.com>
2026-02-14 01:07:36 -0300

    Improve outliner selection

--- src/js/Outliner.js
+++ src/js/Outliner.js
@@ -21,14 +21,30 @@ function Outliner(editor) {
     signals.clearSelection.dispatch()
     editor.selected.forEach((el) => {
       const li = document.getElementById('li' + el.node.id)
-      // el.selectize({ deepSelect: true })
       el.addClass('elementSelected')
-      // li.classList.add('outliner-selected')
+      if (li) li.classList.add('outliner-selected')
     })
     signals.updatedProperties.dispatch()
     // Draw handlers for selected elements
     drawHandlers()
   })
+
+  function drawHandlers() {
+    // Clear existing handlers
+    editor.handlers.clear()
+
+    // Get current zoom level
+    const currentZoom = editor.svg.zoom()
+    const handlerScreenSize = 16 // pixels on screen
+    const handlerWorldSize = handlerScreenSize / currentZoom
+
+    // ... (rest of drawHandlers)
+  }
+
+  // ... (getCoincidentVertices helper is here, I need to skip it to reach clearSelection)
+  // I will use multiple ReplaceChunks to skip the middle part if I can, but I am editing lines 20-31 and 192-203.
+  // Wait, these are far apart. I should use MultiReplaceFileContent.
+
 
   function drawHandlers() {
     // Clear existing handlers
@@ -193,9 +209,8 @@ function Outliner(editor) {
     // Clear handlers
     editor.handlers.clear()
 
-    for (let li of drawingTree.children) {
-      li.classList.remove('outliner-selected')
-    }
+    const selectedItems = drawingTree.querySelectorAll('.outliner-selected')
+    selectedItems.forEach(li => li.classList.remove('outliner-selected'))
     editor.drawing.children().each((el) => {
       // el.selectize(false, { deepSelect: true })
       el.removeClass('elementSelected')
@@ -219,30 +234,35 @@ function Outliner(editor) {
     }
     editor.signals.updatedSelection.dispatch()
   })
-}
-
-function childElements(group, parent) {
-  // console.log('group', group)
-  const ul = document.createElement('ul')
-  const li = document.createElement('li')
-  li.id = 'li' + group.node.id
-  li.textContent = group.node.nodeName + ' ' + group.node.id
-  li.addEventListener('click', () => signals.toogledSelect.dispatch(group))
-  ul.appendChild(li)
-  group.children().each((child) => {
-    // console.log('child', child)
-    if (child.type === 'g') childElements(child, ul)
-    else {
-      const childUl = document.createElement('ul')
-      const li = document.createElement('li')
-      li.id = 'li' + child.node.id
-      li.textContent = child.node.nodeName
-      li.addEventListener('click', () => signals.toogledSelect.dispatch(child))
-      childUl.appendChild(li)
-      ul.appendChild(childUl)
-    }
-  })
-  parent.appendChild(ul)
+  function childElements(group, parent) {
+    // console.log('group', group)
+    const ul = document.createElement('ul')
+    const li = document.createElement('li')
+    li.id = 'li' + group.node.id
+    li.textContent = group.node.nodeName + ' ' + group.node.id
+    li.addEventListener('click', (e) => {
+      e.stopPropagation()
+      signals.toogledSelect.dispatch(group)
+    })
+    ul.appendChild(li)
+    group.children().each((child) => {
+      // console.log('child', child)
+      if (child.type === 'g') childElements(child, ul)
+      else {
+        const childUl = document.createElement('ul')
+        const li = document.createElement('li')
+        li.id = 'li' + child.node.id
+        li.textContent = child.node.nodeName
+        li.addEventListener('click', (e) => {
+          e.stopPropagation()
+          signals.toogledSelect.dispatch(child)
+        })
+        childUl.appendChild(li)
+        ul.appendChild(childUl)
+      }
+    })
+    parent.appendChild(ul)
+  }
 }
 
 export { Outliner }
